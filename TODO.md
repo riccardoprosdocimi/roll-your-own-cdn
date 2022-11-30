cae5f71ebd227ec5e5d206bb562cc0137500589b
Rohit Awate <rohitawate121@gmail.com>
2022-11-30 15:01:36 -0500

    New caching strategy
    Treat memory and disk as same. Promotion only attempted for adding an un-cached article that was grabbed from the origin. No promotion between disk and memory.

--- TODO.md
+++ TODO.md
@@ -10,8 +10,9 @@ DONE:
 - ask professor how to do active measurements without having access to clients
     - WHAT to ping from the HTTP servers?
 
-
-Can we assume client is capable of accepting a gzip compressed file using the Content-Encoding: gzip header?
+TO ASK:
+- Can we assume client is capable of accepting a gzip compressed file using the Content-Encoding: gzip header?
+- How long can the deploy stage take? We want to pre-cache top pages beforehand on disk and load them at runtime. This should take a minute or two during the deploy stage. Is that acceptable?
 
 Add another layer of indirection by not using article names anywhere internally within the cache. Have a dictionary
 at the entry which maps a string to an int or something which is lighter to duplicate within a LookupInfo object
--- cache.py
+++ cache.py
@@ -1,21 +1,26 @@
 import csv
-import gzip
 import heapq
 import os
 from dataclasses import dataclass
 from urllib.parse import quote
 from urllib.request import urlopen
 
+import utils
+
 DEFAULT_ORIGIN_URL = "http://cs5700cdnorigin.ccs.neu.edu:8080"
 
 NOT_CACHED = -1
 ON_DISK = -2
+BUFFER_OFFSET_AUTO = -3
 
 
 @dataclass(order=True)
 class LookupInfo:
     views: int
     buffer_offset: int
+
+    # This is redundant, but sometimes we only have access to
+    # LookupInfo and need to determine what article it is.
     article_name: str
 
     def increment_views(self):
@@ -23,7 +28,7 @@ class LookupInfo:
 
 
 class Cache:
-    def __init__(self, origin_url: str = DEFAULT_ORIGIN_URL):
+    def __init__(self, origin_url: str = DEFAULT_ORIGIN_URL, test_mode: bool = False):
         self.views = {}
         self.heap = []
         self.buffer = []
@@ -32,7 +37,40 @@ class Cache:
         self.max_disk_size = 19 * 1024 * 1024  # conservatively stopping at 19MB
         self.memory_used = 0
         self.max_memory_size = 19 * 1024 * 1024  # conservatively stopping at 19MB
-        self.build_in_memory_cache()
+        self.test_mode = test_mode
+        self.build()
+
+    def build(self):
+        """
+        We go through the CSV file as a starting point and then load
+        those articles from disk. We fit as many as we can into memory
+        and let the rest remain on disk. The ones that are moved to
+        memory get deleted from the disk.
+
+        This will lead to free space on disk that can be used for caching
+        articles that might get fetched from origin.
+        """
+        with open("pageviews.csv") as article_file:
+            reader = csv.DictReader(article_file)
+            for row in reader:
+                article = quote(row["article"].replace(" ", "_"))
+                views = int(row["views"])
+
+                if os.path.exists(f"cache/{article}"):
+                    compressed_article = self.get_from_disk_cache(article)
+
+                    lookup_info = self.put(article, compressed_article, views)
+                    if lookup_info.buffer_offset == NOT_CACHED:
+                        # Don't break, continue so that any potentially
+                        # smaller article down the line can be cached.
+                        continue
+
+                    # Remove from disk cache if loaded into memory
+                    if not self.test_mode and lookup_info.buffer_offset != ON_DISK:
+                        os.remove(f"cache/{article}")
+                else:
+                    lookup_info = LookupInfo(views, NOT_CACHED, article)
+                    self.views[article] = lookup_info
 
     def get(self, article: str) -> bytes:
         lookup_info: LookupInfo = self.views[article]
@@ -42,13 +80,13 @@ class Cache:
             # (CACHE MISS) fetch from origin and cache to disk
             print(f"{article}: Not cached, fetching from origin")
 
+            # Fetch article from origin and compress it
             article_raw_bytes = self.fetch_from_origin(article)
-            compressed_article = self.compress_article(article_raw_bytes)
+            compressed_article = utils.compress_article(article_raw_bytes)
 
-            if self.save_to_disk_cache(article, compressed_article):
-                lookup_info.buffer_offset = ON_DISK
-            else:
-                print("Failed to cache article to disk")
+            # Optimistically cache it to disk if we have the space
+            if self.put(article, article_raw_bytes, self.views[article]) == NOT_CACHED:
+                self.attempt_promotion(article, article_raw_bytes)
 
             return compressed_article
         elif lookup_info.buffer_offset == ON_DISK:
@@ -56,8 +94,6 @@ class Cache:
             print(f"{article}: Serving from disk cache")
 
             compressed_article = self.get_from_disk_cache(article)
-            self.attempt_promotion(article, compressed_article)
-
             return compressed_article
         else:
             # (IN-MEMORY CACHE HIT) fetch from in-memory cache
@@ -65,82 +101,58 @@ class Cache:
 
             return self.buffer[lookup_info.buffer_offset]
 
-    def build_in_memory_cache(self):
-        with open("pageviews.csv") as article_file:
-            reader = csv.DictReader(article_file)
-            for row in reader:
-                article = quote(row["article"].replace(" ", "_"))
-                views = int(row["views"])
+    def put(self, article: str, article_raw_bytes: bytes, views: int) -> LookupInfo:
+        buffer_offset = self.add_to_in_memory_cache(article_raw_bytes)
+        if buffer_offset == NOT_CACHED:
+            buffer_offset = self.add_to_disk_cache(article, article_raw_bytes)
 
-                try:
-                    with open(f"cache/{article}", "rb") as fd:
-                        compressed_article = fd.read()
+        lookup_info = LookupInfo(views, buffer_offset, article)
+        if lookup_info.buffer_offset != NOT_CACHED:
+            self.heap.append(lookup_info)
+            self.views[article] = lookup_info
 
-                        if not self.fits_in_memory_cache(compressed_article):
-                            lookup_info = LookupInfo(views, ON_DISK, article)
+        return lookup_info
 
-                            # Don't break, continue so that any potentially smaller article
-                            # down the line can be cached.
-                            continue
+    def add_to_in_memory_cache(
+        self, article_raw_bytes: bytes, buffer_offset: int = BUFFER_OFFSET_AUTO
+    ) -> int:
+        if self.fits_in_memory_cache(article_raw_bytes):
+            self.memory_used += len(article_raw_bytes)
+            if buffer_offset == BUFFER_OFFSET_AUTO:
+                self.buffer.append(article_raw_bytes)
+                return len(self.buffer) - 1
+            else:
+                self.buffer[buffer_offset] = article_raw_bytes
+                return buffer_offset
 
-                        self.memory_used += len(compressed_article)
-                        self.buffer.append(compressed_article)
+        return NOT_CACHED
 
-                        # Remove from disk cache
-                        os.remove(f"cache/{article}")
+    def remove_from_in_memory_cache(self, article: str) -> int:
+        """
+        Since buffer is a list, there's really nothing to "remove" here.
+        We can set the element at the offset in the buffer to None in the
+        hopes that the garbage collector reclaims that memory.
 
-                        buffer_offset = len(self.buffer) - 1
-                        lookup_info = LookupInfo(views, buffer_offset, article)
-                except IOError:
-                    lookup_info = LookupInfo(views, NOT_CACHED, article)
-                finally:
-                    self.views[article] = lookup_info
-                    self.heap.append(lookup_info)
+        We return the buffer_offset that is now empty in the hopes that it
+        will be refilled with a new article. This method is only called by
+        attempt_promotion which does exactly that, so we're safe. Otherwise,
+        we would have holes in our buffer.
 
-        heapq.heapify(self.heap)
+        :return: buffer_offset: offset within the in-memory buffer that the article occupied
+        """
+        lookup_info = self.views[article]
+        self.memory_used -= len(self.buffer[lookup_info.buffer_offset])
+        self.buffer[lookup_info.buffer_offset] = None
+        return lookup_info.buffer_offset
 
-    def attempt_promotion(self, article_name_to_promote, compressed_article_to_promote):
-        lookup_info_to_demote: LookupInfo = heapq.nsmallest(1, self.heap)[0]
-        lookup_info_to_promote: LookupInfo = self.views[article_name_to_promote]
-
-        eligible = lookup_info_to_demote.views < lookup_info_to_promote.views
-
-        if eligible:
-            compressed_article_to_demote = self.buffer[
-                lookup_info_to_demote.buffer_offset
-            ]
-
-            swap_possible_on_disk = (
-                self.disk_used
-                - len(compressed_article_to_promote)
-                + len(compressed_article_to_demote)
-            ) <= self.max_disk_size
-
-            swap_possible_in_memory = (
-                self.memory_used
-                - len(compressed_article_to_demote)
-                + len(compressed_article_to_promote)
-            ) <= self.max_memory_size
-
-            if swap_possible_on_disk and swap_possible_in_memory:
-                self.remove_from_disk_cache(article_name_to_promote)
-                self.save_to_disk_cache(
-                    lookup_info_to_demote.article_name, compressed_article_to_demote
-                )
-                self.buffer[
-                    lookup_info_to_demote.buffer_offset
-                ] = compressed_article_to_promote
-                lookup_info_to_demote.buffer_offset = ON_DISK
-                lookup_info_to_promote.buffer_offset = (
-                    lookup_info_to_demote.buffer_offset
-                )
-                print(
-                    f"Promoted {article_name_to_promote}, demoted {lookup_info_to_demote.article_name}"
-                )
-
-    def fetch_from_origin(self, article: str) -> bytes:
-        with urlopen(f"{self.origin_url}/{article}") as response:
-            return response.read()
+    def add_to_disk_cache(self, article: str, article_raw_bytes: bytes) -> int:
+        try:
+            with open(f"cache/{article}", "wb") as cache_file:
+                cache_file.write(article_raw_bytes)
+                self.disk_used += len(article_raw_bytes)
+                return ON_DISK
+        except IOError:
+            return NOT_CACHED
 
     def remove_from_disk_cache(self, article: str):
         filepath = f"cache/{article}"
@@ -148,28 +160,120 @@ class Cache:
         os.remove(filepath)
         self.disk_used -= file_size
 
+    def attempt_promotion(
+        self, article_name_to_promote: str, compressed_article_to_promote: bytes
+    ) -> bool:
+        """
+        Promotion: When the cache is full, and we need to add a new article to it.
+
+        In this situation, we find the article with minimum views from our cache and compare its
+        views with that of the new article. If the new article has more views, we perform further
+        checks to ensure the new article can fit in the cache (memory or disk, doesn't matter)
+        once the old one is evicted. If that check succeeds, we evict the one in the cache and add
+        the new one in its place.
+
+        To make the lookup of the article with minimum views faster, we employ a heap. We run heapify
+        lazily i.e. only before we need to grab the minimum element. At other times, we don't need the
+        heap. This results in O(logn) heapify complexity, which is not too bad for the total number of
+        articles we anticipate in the cache (~400). The logn runtime is justified because now we can
+        look up the article with minimum views in O(1).
+        """
+        heapq.heapify(self.heap)
+        lookup_info_to_evict: LookupInfo = heapq.heappop(self.heap)
+        lookup_info_to_promote: LookupInfo = self.views[article_name_to_promote]
+
+        eligible = lookup_info_to_evict.views < lookup_info_to_promote.views
+
+        if not eligible:
+            # If we can't evict, add back to the heap
+            self.heap.append(lookup_info_to_evict)
+            return False
+        else:
+            compressed_article_to_evict = self.buffer[
+                lookup_info_to_evict.buffer_offset
+            ]
+
+            swap_possible_in_memory = (
+                self.memory_used
+                - len(compressed_article_to_evict)
+                + len(compressed_article_to_promote)
+            ) <= self.max_memory_size
+
+            if swap_possible_in_memory:
+                lookup_info_to_promote.buffer_offset = (
+                    lookup_info_to_evict.buffer_offset
+                )
+                lookup_info_to_evict.buffer_offset = NOT_CACHED
+
+                deleted_buffer_offset = self.remove_from_in_memory_cache(
+                    lookup_info_to_evict.article_name
+                )
+                self.add_to_in_memory_cache(
+                    compressed_article_to_promote,
+                    deleted_buffer_offset,
+                )
+
+                print(
+                    f"Promoted {article_name_to_promote}, evicted {lookup_info_to_evict.article_name}"
+                )
+
+                return True
+
+            swap_possible_on_disk = (
+                self.disk_used
+                - len(compressed_article_to_promote)
+                + len(compressed_article_to_evict)
+            ) <= self.max_disk_size
+
+            if swap_possible_on_disk:
+                lookup_info_to_evict.buffer_offset = NOT_CACHED
+                lookup_info_to_promote.buffer_offset = ON_DISK
+
+                # This is the damn reason why we need to have a redundant
+                # reference to article_name within LookupInfo.
+                self.remove_from_disk_cache(lookup_info_to_evict.article_name)
+                self.add_to_disk_cache(
+                    lookup_info_to_promote.article_name, compressed_article_to_promote
+                )
+
+                print(
+                    f"Promoted {article_name_to_promote}, evicted {lookup_info_to_evict.article_name}"
+                )
+
+                return True
+
+            return False
+
+    def fetch_from_origin(self, article: str) -> bytes:
+        with urlopen(f"{self.origin_url}/{article}") as response:
+            return response.read()
+
     def fits_in_memory_cache(self, article_raw_bytes: bytes) -> bool:
         return self.memory_used + len(article_raw_bytes) <= self.max_memory_size
-
-    def save_to_disk_cache(self, article: str, article_raw_bytes: bytes) -> bool:
-        try:
-            with open(f"cache/{article}", "wb") as cache_file:
-                cache_file.write(article_raw_bytes)
-                self.disk_used += len(article_raw_bytes)
-                return True
-        except IOError:
-            return False
 
     @staticmethod
     def get_from_disk_cache(article: str) -> bytes:
         with open(f"cache/{article}", "rb") as fd:
             return fd.read()
 
-    @staticmethod
-    def compress_article(article_raw_bytes: bytes):
-        return gzip.compress(article_raw_bytes)
-
 
 if __name__ == "__main__":
-    cache = Cache()
-    print(cache.memory_used, cache.disk_used)
+    cache = Cache(test_mode=True)
+    print(cache.memory_used // 1024 / 1024, cache.disk_used // 1024 / 1024)
+
+    from timeit import default_timer as timer
+
+    start = timer()
+    cache.get("Rishi_Sunak")
+    end = timer()
+    print(f"{(end - start) * 1000:.2f}ms")
+
+    start = timer()
+    cache.get("Prabhas")
+    end = timer()
+    print(f"{(end - start) * 1000:.2f}ms")
+
+    start = timer()
+    cache.get("Jeff_Bridges")
+    end = timer()
+    print(f"{(end - start) * 1000:.2f}ms")
